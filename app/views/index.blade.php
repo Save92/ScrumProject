a3913ca914d73b13bbc132a5b040b75549b7cc52
Save92 <nico_la92@hotmail.com>
2014-03-14 12:08:58 +0100

    Modification données page accueil

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,8 +3,37 @@
 
 <p class="well well-lg">
 	<strong>Ad Min</strong><br/>
+	Role : <strong>Administrateur</strong>
 	Email <strong>a@a.a</strong><br/>
 	Password <strong>a</strong>
+</p>
+
+<p class="well well-lg">
+	<strong>Lorem Ipsum</strong><br/>
+	Role : <strong>Secrétaire Pédagogique</strong>
+	Email <strong>lo@re.m</strong><br/>
+	Password <strong>azerty</strong>
+</p>
+
+<p class="well well-lg">
+	<strong>Claire Pedurand</strong><br/>
+	Role : <strong>Professeur</strong>
+	Email <strong>p@p.p</strong><br/>
+	Password <strong>p</strong>
+</p>
+
+<p class="well well-lg">
+	<strong>Charlotte Alarmée</strong><br/>
+	Role : <strong>Etudiant</strong>
+	Email <strong>e@e.e</strong><br/>
+	Password <strong>e</strong>
+</p>
+
+<p class="well well-lg">
+	<strong>Yann Bescond</strong><br/>
+	Role : <strong>Candidat</strong>
+	Email <strong>e3@e.e</strong><br/>
+	Password <strong>e</strong>
 </p>
 
 @stop
